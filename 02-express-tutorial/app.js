3707b89d568c39db19f25143858e41e54ebf153a
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2023-11-28 13:05:50 -0500

    worked on creating a simple http server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,24 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("req is :", req.method, req.url);
+
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, "successful response", { "content-type": "text/html" });
+    res.write("<h1 style='color:green'>Home Page</h1>", "utf8");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, "successful response", { "content-type": "text/html" });
+    res.write("<h1 style='color:green'>About Page</h1>", "utf8");
+    res.end();
+  } else {
+    res.writeHead(404, "unsuccessful response", {
+      "content-type": "text/html",
+    });
+    res.write("<h1 style='color:red'>Page Not found</h1>", "utf8");
+    res.end();
+  }
+});
+
+server.listen("5000");
