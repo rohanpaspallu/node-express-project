900644b58f83de4469dfb4e8b005203b0539e50b
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2023-11-29 18:10:39 -0500

    worked with passing parameters to the url to get some information

---
+++ 02-express-tutorial/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { products } = require("./data");
+const app = express();
+
+//SSR
+const showEachItem = (product) => {
+  return `<div>
+    <h1>Product Name: ${product.name}</h1>
+    <img src='${product.image}' />
+    <h2>Price: $${product.price}</h2>
+    <p><strong>Description</strong> ${product.desc}</p>
+  </div>`;
+};
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hope page</h1><a href='/api/products'>Go to product list</a>");
+  // res.json(products);
+});
+
+//Get a list of all the products
+app.get("/api/products", (req, res) => {
+  const test = products.map((product) => showEachItem(product));
+
+  console.log("value of test is :", test);
+  const newProducts = products.map((product) => {
+    return { id: product.id, name: product.name, image: product.image };
+  });
+  res.json(newProducts);
+  // res.send(test.toString());
+  // res.send("<h1>Products page</h1>");
+});
+
+//Get only one product with id
+app.get("/api/products/:id", (req, res) => {
+  console.log(req.params.id);
+  const findProduct = products.find((item) => item.id == req.params.id);
+  console.log(findProduct);
+  findProduct
+    ? res.send(findProduct)
+    : res.status(404).send("Product Doesnt Exist");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on 5000....");
+});
